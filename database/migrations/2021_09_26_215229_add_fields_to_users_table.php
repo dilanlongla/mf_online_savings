d9371c91052ef867bf89d3e5cb3107c782c2efa7
dilanlongla <44490676+dilanlongla@users.noreply.github.com>
2021-09-26 23:03:30 +0100

    added fields to users table via migration

---
+++ database/migrations/2021_09_26_215229_add_fields_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('matricule', 25);
+            $table->string('nic', 25);
+            $table->text('address');
+            $table->integer('tel');
+            $table->string('occupation', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
