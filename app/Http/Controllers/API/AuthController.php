e28e3867bde747d5837ef212231fa11b644e9c4c
dilanlongla <44490676+dilanlongla@users.noreply.github.com>
2021-09-27 01:23:51 +0100

    register methods created

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -23,6 +23,102 @@ class AuthController extends ApiController
             'password' => 'required',
             'c_password' => 'required|same:password',
         ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+   
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyApp')->accessToken;
+        $success['name'] =  $user->name;
+   
+        return $this->sendResponse($success, 'User register successfully.');
+    }
+    
+    /**
+     * Register client api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function registerClient(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'nic' => 'required',
+            'address' => 'required',
+            'tel' => 'required|numeric',
+            // 'password' => 'required',
+            // 'c_password' => 'required|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+        $input = $request->all();
+        $input['name'] = $input['first_name'] . ' ' . $input['last_name'];
+
+        $user = User::create($input);
+        $user->assignRole('client');
+        $success['name'] =  $user->name;
+
+        activity()
+            ->causedBy(Auth()->user())
+            ->log('New client registered successfully');
+        return $this->sendResponse($success, 'New client registered successfully.');
+    }
+
+    /**
+     * Register agent api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function registerAgent(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'nic' => 'required',
+            'matricule' => 'required',
+            'address' => 'required',
+            'tel' => 'required|numeric',
+            'email' => 'required|email',
+            // 'password' => 'required',
+            // 'c_password' => 'required|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $input = $request->all();
+        $input['name'] = $input['first_name'] . ' ' . $input['last_name'];
+        $user = User::create($input);
+        $user->assignRole('agent');
+        $success['token'] =  $user->createToken('MyApp')->accessToken;
+        $success['name'] =  $user->name;
+
+        activity()
+            ->causedBy(Auth()->user())
+            ->log('New agent registered');
+        return $this->sendResponse($success, 'New Agent registered successfully.');
+    }
+
+    /**
+     * Register agent api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function passwordUpdate($id, Request $request)
+    {
+        $user = User::whereId($id);
+
+        $validator = Validator::make($request->all(), [
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
 
         if ($validator->fails()) {
             return $this->sendError('Validation Error.', $validator->errors());
@@ -30,12 +126,15 @@ class AuthController extends ApiController
 
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-        $success['token'] =  $user->createToken('MyApp')->accessToken;
+        $user->update($input);
         $success['name'] =  $user->name;
 
+        activity()
+            ->causedBy($user)
+            ->log('Password updated successfully');
         return $this->sendResponse($success, 'User register successfully.');
     }
+
 
     /**
      * Login api
@@ -49,6 +148,9 @@ class AuthController extends ApiController
             $success['token'] =  $user->createToken('MyApp')->accessToken;
             $success['name'] =  $user->name;
 
+            activity()
+                ->causedBy($user)
+                ->log('User logged in successfully');
             return $this->sendResponse($success, 'User login successfully.');
         } else {
             return $this->sendError('Unauthorised.', ['error' => 'Unauthorised']);
